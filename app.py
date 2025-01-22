9330348e03953f8902c7ef4d0950523d323c54e0
sbakkali01 <bakkalisami01@gmail.com>
2025-01-22 19:37:20 +0100

    File uploading mechanism complete

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template
+import os
+from time import sleep
 
-app=Flask(__name__)
+from flask import Flask, render_template, request, redirect, flash, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
-@app.route('/')
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "/home/sami/PycharmProjects/Smart Import/upload"
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SECRET_KEY"]="ayo"
+
+
+def allowed_file(filename: str):
+    return '.' in filename and filename.split('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route('/', methods=["GET", "POST"])
 def main_page():
-    return render_template("index.html")
+    if request.method == "POST":
+        if 'file_input' not in request.files:
+            flash("No file was sent !")
+            return redirect(url_for("main_page"))
+        file = request.files["file_input"]
+        if not allowed_file(file.filename):
+            flash("The file format is not supported !")
+            return redirect(url_for("main_page"))
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('download_file', name=filename))
+    else:
+        return render_template("index.html")
